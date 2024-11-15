dde4df01825df03a72e7c3969e92b4d617f9845a
Serge-Mane <sergemane81@gmail.com>
2024-11-15 13:07:17 +0000

    le choix de l'utilisateur

--- src/tech/sam/surfaces/utilities/ReaderUtilities.java
+++ src/tech/sam/surfaces/utilities/ReaderUtilities.java
@@ -1,4 +1,21 @@
 package tech.sam.surfaces.utilities;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ReaderUtilities {
+
+    public int readChoice() throws IOException {
+        this.displayInstructions();
+        BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+        return Integer.parseInt(reader.readLine());
+    }
+
+    private void displayInstructions(){
+        System.out.println("Bienvenue dans ce programme pour calculer les surfaces");
+        System.out.println("Pour calculer la surface du Cercle taper 1");
+        System.out.println("Pour calculer la surface du Rectangle/carre taper 2");
+        System.out.println("Pour calculer la surface du Triangle taper 3");
+    }
 }
