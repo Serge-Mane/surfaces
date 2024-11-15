523aca5d4f83cab1cce471ecf854add986fe8ed3
Serge-Mane <sergemane81@gmail.com>
2024-11-15 19:53:15 +0000

    lire les donneess pour un cercle

--- src/tech/sam/surfaces/Main.java
+++ src/tech/sam/surfaces/Main.java
@@ -1,8 +1,19 @@
 package tech.sam.surfaces;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+import tech.sam.surfaces.utilities.ReaderUtilities;
 
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        ReaderUtilities readerUtilities=new ReaderUtilities();
+        List<Integer> coordinates=readerUtilities.readData();
+        /*
+        for (int i=0;i<coordinates.size();i++){
+            System.out.println(String.format("coordonees %s vaut %s",i,coordinates.get(i)));
+        }*/
+
+        coordinates.forEach(item-> System.out.println(String.format("coordonees %s",item)));
     }
 }
--- src/tech/sam/surfaces/utilities/ReaderUtilities.java
+++ src/tech/sam/surfaces/utilities/ReaderUtilities.java
@@ -3,10 +3,43 @@ package tech.sam.surfaces.utilities;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReaderUtilities {
+/*
+* lire les donnees
+* retourner les coordonnees
+* @throw IOException
+ * */
+    public List<Integer> readData() throws IOException {
+        int choice=this.readChoice();
+        List<Integer> coordinates=new ArrayList<>();
+        BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
 
-    public int readChoice() throws IOException {
+        if (choice==1){
+            System.out.println("Pour le cercle saisi 4 coordonnes");
+            int index=0;
+            while (index < 4){
+                System.out.println("coordonnees"+(index +1));
+                String valueAsString=reader.readLine();
+                if (!valueAsString.isEmpty()){
+                    int value=Integer.parseInt(valueAsString);
+                    coordinates.add(value);
+                    index++;
+                }
+               }
+        } else if (choice==2) {
+            System.out.println("Pour Rectangle/carre saisi 6 coordonnes");
+        }else if (choice==3) {
+            System.out.println("Pour Triangle 6 coordonnes");
+        }else {
+            System.out.println("Votre choix est inconnu");
+        }
+        return coordinates;
+    }
+
+    private int readChoice() throws IOException {
         this.displayInstructions();
         BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
         return Integer.parseInt(reader.readLine());
@@ -18,4 +51,6 @@ public class ReaderUtilities {
         System.out.println("Pour calculer la surface du Rectangle/carre taper 2");
         System.out.println("Pour calculer la surface du Triangle taper 3");
     }
+
+
 }
