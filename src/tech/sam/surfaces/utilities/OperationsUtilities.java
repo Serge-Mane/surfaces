3580266dafb56054d6a8300779bae1570776b609
Serge-Mane <sergemane81@gmail.com>
2024-11-20 12:42:37 +0000

    les methodes protected

--- src/tech/sam/surfaces/utilities/OperationsUtilities.java
+++ src/tech/sam/surfaces/utilities/OperationsUtilities.java
@@ -3,7 +3,8 @@ package tech.sam.surfaces.utilities;
 import tech.sam.surfaces.entities.Point;
 
 public class OperationsUtilities {
-    public double distance(Point PointOne, Point pointTwo){
+    //seul les classes filles qui heritent de cette classe qui peuvent utiliser la methode protected
+    protected double distance(Point PointOne, Point pointTwo){
         double x= Math.pow((pointTwo.getX()- PointOne.getX()), 2);
         double y= Math.pow((pointTwo.getY()- PointOne.getY()), 2);
         return Math.sqrt(x + y);
